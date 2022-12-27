a66f79d2bd28240e2a83df70f591844797d972c3
Hanna Manko <71004637+hmanko@users.noreply.github.com>
2022-12-27 12:12:48 +0100

    Add files via upload

--- Patches_creation_finalCorrection(inWork).py
+++ Patches_creation_finalCorrection(inWork).py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 22 16:14:36 2021
-https://github.com/hmanko/NN_sr
+https://github.com/hmanko/srSMLM_DeepL
 @author: hannamanko
 """
+
+#
+#
+#
 #####################  Import the required libraries  ###################
 import numpy as np
 import pandas as pd
@@ -31,7 +35,7 @@ half_width = int(loc_image_width/2)    ##
 
 ##################### DEFINING THE  FUNCTIONS ###################
 ###
-##### #######    Useful  function to plot on one figure multiple patchs
+##### #######    Useful  function to plot on one figure multiple patches
 def show_images(patches):
     n = int(math.sqrt(len(patches))+2)
     m = int(math.sqrt(len(patches)))
@@ -46,9 +50,9 @@ def show_images(patches):
 def set_coordinates(i, half_width, loc_sp_distance, sp_image_width):
     row = np.array(where(cluster == i))
     ## coordinates for localization part
-    loc_sp_dist_shift = random.randint(-15,5)  
-    shift_y = random.randint(-5,5)   ##    shift for x and y coordinates. To move the box around localization
-    shift_x = random.randint(-5,5)   ##
+    loc_sp_distance = loc_sp_distance+random.randint(-10,20)  
+    shift_y = random.randint(-6,6)   ##    shift for x and y coordinates. To move the box around localization
+    shift_x = random.randint(-6,6)   ##
    # shift_y = 0   ##      if shift is not requires. When we need to create patches 
    # shift_x = 0   ##
     y1 = int(dat[np.array(row)[0,0],0]-half_width-shift_y)   #   start y-coordinate of the box around localization
@@ -58,9 +62,9 @@ def set_coordinates(i, half_width, loc_sp_distance, sp_image_width):
     ## coordinates for spectral part
     x = dat[np.array(row)[0,0],0]
     y = dat[np.array(row)[0,0],1]
-    y11 = int(dat[np.array(row)[0,0],0]+loc_sp_distance+loc_sp_dist_shift-shift_y)    #   start y-coordinate of the box around spectra
-    y21 = int(dat[np.array(row)[0,0],0]+loc_sp_distance+loc_sp_dist_shift + sp_image_width-shift_y) #   end y-coordinate of the box around spectra
-    return(y1,y2,x1,x2,y11,y21,row,x,y,loc_sp_dist_shift, shift_y, shift_x)
+    y11 = int(dat[np.array(row)[0,0],0]+loc_sp_distance-shift_y)    #   start y-coordinate of the box around spectra
+    y21 = int(dat[np.array(row)[0,0],0]+loc_sp_distance + sp_image_width-shift_y) #   end y-coordinate of the box around spectra
+    return(y1,y2,x1,x2,y11,y21,row,x,y)
 
 ####   The code was first writen to be able to create patches of different types
 ##   Function that returns image shape depending on patch type chosen
@@ -86,9 +90,9 @@ def patches_formation(row, stack):
         patch = patch.reshape(row.shape[1],16,48)        
     if combined == True:
         patch = np.concatenate((np.float64(stack[frame, x1:x2, y1:y2]), 
-                                np.float64(stack[frame, x1-2-sp_cor:x2-2-sp_cor, y11:y21])), axis=3)
+                                np.float64(stack[frame, x1-sp_cor:x2-sp_cor, y11:y21])), axis=3)
         patch = patch.reshape(row.shape[1],16,64)
-    return(patch, sp_cor)
+    return(patch)
 
 #######   Function to calculate Spatial Frecuency as described in (Shutao Li et al. 
 # 'Combination of images with diverse focuses using the spatial frequency',  Information Fusion
@@ -140,7 +144,8 @@ def SF_part(n, patch_x, pat_s):
     fr.pop(n)
     pat_s = SF_Image(patch_x[((fr)),:,:], combined=True)
     return (pat_s)    
-   
+ 
+
 ############################################################### 3
 ############################################################### 2
 ############################################################### 1 ... 
@@ -151,42 +156,19 @@ only_loc = False
 combined = True
 ###\\\
 ##########################
-'''
-start_time = time.time()
-print("--- %s seconds ---" % (time.time() - start_time))
 
-### Downloading PeakFit file in csv format with deleted first 7 rows and # before Frame
-data = pd.read_csv('/Users/hannamanko/Desktop/diff_st/My Library_2/U-net/patches/Gqaunt_100mW_9.csv')
-data = pd.read_csv('D:/My Library_2/U-net/for figures/50mW_4_MMStack_Pos0.ome-2.tif.results.csv')
-'''
-
-# Reading the localisation file created in one of the next plugins (line , line)
-#data = pd.read_csv('/Users/hannamanko/Desktop/diff_st/My Library_2/U-net/for figures/50mW_4_THSt_5.0.csv')
-
-data = pd.read_csv('/Users/hannamanko/Desktop/NN_data/100mW_2_MMStack_Pos0.ome.tif.results.csv')
-
-data = pd.read_csv('/Users/hannamanko/Desktop/diff_st/Our_Papers/Unet_SRsmlm/data/50mW_1_MMStack_Pos0.ome.tif.results.csv')
+data = pd.read_csv('/path/25mW_2_MMStack_Pos0.ome.tif.results.csv')
 
 markerSize = 0.05   
-####    localization file from ThunderSTORM (Fiji plugin)  ###
-# ////_______
-data = data[data["Intensity [photon]"]>10000]  ## you can filter the data by intensity to discard noise localizations   
-
-plt.plot(data["x [nm]"], data["y [nm]" ], 'o', markersize=markerSize, color='red')
-
-datat = pd.concat([pd.DataFrame(data["x [nm]"]),pd.DataFrame(data["y [nm]"]),pd.DataFrame(data["frame"])], axis = 1, ignore_index=False)
-datat['y [nm]'] = (datat['y [nm]']/21.36752)/5
-datat['x [nm]'] = (datat['x [nm]']/21.36752)/5
-### \\\\_______
-  
+ 
 
 ####    localization file from PeakFit (part of GDSC SMLM2 plugin in Fiji)  ###
-# /////_____
+# /////
 data = data[data["Signal"]>5000]
 plt.plot(data["origX"], data["origY" ], 'o', markersize=markerSize, color='red')
 datat = pd.concat([pd.DataFrame(data["origX"]),pd.DataFrame(data["origY"]),pd.DataFrame(data["Frame"])], axis = 1, ignore_index=False)
 
-### \\\\\_______
+### \\\\\
 
 data_n= datat.to_numpy()
 d = data_n[(data_n[:,0]>10)&(data_n[:,0]<220)&(data_n[:,1]>10)&(data_n[:,1]<250)]
@@ -194,8 +176,6 @@ plt.plot(d[:,0], d[:,1], 'o', markersize=markerSize, color='red')
 
 data = data[(data['origX']>10)&(data['origX']<220)&(data['origY']>10)&(data['origY']<250)]
 data = data.reset_index()
-
-
 #####
 ##############################################################################
 #####       Next we cluster the localizations on consecutive frames to create 'clean' images from the same localization
@@ -237,14 +217,11 @@ plt.show()
 
 data['clusterID'] = pd.DataFrame(cluster)
 
-
+################################################
 #  Reading the original stack of aquired images
-#stack = imread("/Users/hannamanko/Desktop/diff_st/My Library_2/U-net/for figures/50mW_4_MMStack_Pos0.ome-1.tif")
 
-stack = imread("/Users/hannamanko/Desktop/NN_data/100mW_2/100mW_2_MMStack_Pos0.ome.tif")
-
-stack = imread("/Users/hannamanko/Desktop/diff_st/Our_Papers/Unet_SRsmlm/data/150mW_2/150mW_2_MMStack_Pos0.ome.tif")
-stack = stack/60000
+stack = imread("/path/25mW_2_MMStack_Pos0.ome.tif")
+stack = stack/2**16 ####   Normalization
 
 start_time = time.time()
 print("--- %s seconds ---" ,(time.time() - start_time))   # printing start time
@@ -254,21 +231,18 @@ im_shape = image_type(image_height, sp_image_width,loc_image_width)   # Reading 
 count = 0   
 print("Please, wait until the end")
 start_time = time.time()
+x1_list = []
+y1_list = []
 SF_p_list = []
 SF_sum_list = []
-loc_sp_dist_shift_list = []
-shift_y_list = []
-shift_x_list = []
-sp_cor_list = []
 data_v = pd.DataFrame()
 for i in clusters:  
-    #print('i = ',i)    #  for each cluster index
-    y1,y2,x1,x2,y11,y21,row,x,y,loc_sp_dist_shift,shift_y,shift_x = set_coordinates(i,half_width,loc_sp_distance, sp_image_width)  #  defining the cordinates of the box
+    y1,y2,x1,x2,y11,y21,row,x,y = set_coordinates(i,half_width,loc_sp_distance, sp_image_width)  #  defining the cordinates of the box
     patch_x = np.zeros((np.shape(row[0])[0], im_shape[0], im_shape[1]))  # creating the empty patch of defined size
     try:
-        patch, sp_cor = patches_formation(row, stack) 
+        patch = patches_formation(row, stack) 
     except:
-        continue 
+        continue
     patch_x = patch
     pat_s = np.zeros(patch_x.shape)
     SF_p = []
@@ -281,12 +255,10 @@ for i in clusters:
                                                    (np.array(SF_sum)/np.array(SF_p)).min())/2) #    
     patch_ = patch_x[ind]
     patch_sum  = pat_s[ind]
+    x1_list.extend([x]*len(ind[0]))
     SF_p_list.extend(np.array(SF_p)[ind[0]])
     SF_sum_list.extend(np.array(SF_sum)[ind[0]])
-    loc_sp_dist_shift_list.extend([loc_sp_dist_shift]*len(ind[0]))
-    shift_y_list.extend([shift_y]*len(ind[0]))
-    shift_x_list.extend([shift_x]*len(ind[0]))
-    sp_cor_list.extend([sp_cor]*len(ind[0]))
+    y1_list.extend([y]*len(ind[0]))
     data_v = data_v.append(data.iloc[list(row[0][np.array(ind)][0])], ignore_index = True)
     if count == 0:                                                     #   gethering all the patches in one big set
         patch__ = patch_                                               #   set of original images      
@@ -299,50 +271,51 @@ for i in clusters:
 print("--- %s seconds ---" % (time.time() - start_time))    #  printing  end time 
 
 
-data_v['SF_raw'] = SF_p_list
-data_v['SF_clean'] = SF_sum_list
-data_v['shift_x'] = shift_x_list
-data_v['shift_y'] = shift_y_list
-data_v['loc_sp_dist_shift'] = loc_sp_dist_shift_list
-data_v['sp_cor'] = sp_cor_list
+#  Now we can save created raw and 'clean' sets of patches
+#coordinates = np.zeros((len(x1_list),2))
+#coordinates[:,0]= x1_list
+#coordinates[:,1]=y1_list
+
+#data_vv = pd.DataFrame()
+#for i in range(0,len(patch__)):
+#    r = where((data['origX'] == coordinates[i,0])&(data['origY']==coordinates[i,1])&(data['origValue']==patch__[0].max()*60000))
+#    data_vv = data_vv.append(data.iloc[r[0][0],:])  
+
+#pd.DataFrame(coordinates).to_csv('/Users/hannamanko/Desktop/coordinates_50_1.csv',index = False)
+
 
 patch__v = patch__.reshape(len(patch__),64*16)
 patch_sum_v = patch_sum_.reshape(len(patch_sum_),64*16)
 
 data_vv = pd.concat((data_v,pd.DataFrame(patch__v), pd.DataFrame(patch_sum_v)), axis = 1)
 
-###  This is additional filtering
-##   To discard patches with too low intensity in localisation part,
-#    These are mostly kind of noisy patches
 ind = []
 for i in range(0, len(patch__)):
-    if patch__[i,:16,:].max() <0.09:   
+    if patch__[i,:16,:].max() <0.09:
         ind.append(i)
 patch___ = np.delete(patch__, [ind], axis=0)
 patch_sum__ = np.delete(patch_sum_, [ind], axis=0)
 data_vv = data_vv.drop(data_vv.index[ind])
 
 
-data_vv.to_csv('/Users/hannamanko/Desktop/NN_data/data_100_2.csv', sep = ';', decimal='.', index = False,encoding="utf-8")
+data_vv.to_csv('/path/data_150_2.csv', sep = ';', decimal='.', index = False,encoding="utf-8")
+imsave('/path/150_2_patch_sum.tif', patch_sum__)
+imsave('/path/150_2_patch.tif', patch___) 
 
-
-dat = pd.read_csv('/Users/hannamanko/Desktop/NN_data/data_100_2.csv')
-####   Also patches can be saved as *.tif files
-imsave('/Users/hannamanko/Desktop/NN_data/25_2_patch_sum.tif', patch_sum__)
-imsave('/Users/hannamanko/Desktop/NN_data/25_2_patch.tif', patch___) 
+plt.imshow(np.array(data_vv.iloc[1,16:1040]).reshape(16,64))
 
 #////
 
-'''
-pred = imread('/Users/hannamanko/Desktop/NN_data/150_2_pred_25.tif')
-data = pd.read_csv('/Users/hannamanko/Desktop/diff_st/Our_Papers/Unet_SRsmlm/data/data_pred_150_2.csv', sep = ';', decimal='.')
+
+pred = imread('/path/150_2_pred_25.tif')
+data = pd.read_csv('/path/data_pred_150_2.csv', sep = ';', decimal='.')
 
 data_ = data.drop(data.columns[list(range(16,data.shape[1]))], axis=1)
 pred_v = pred.reshape(len(pred),64*16)
 data_ = pd.concat((data_,pd.DataFrame(pred_v)), axis = 1)
 
-data_.to_csv('/Users/hannamanko/Desktop/NN_data/data_pred_150_2.csv', sep = ';', decimal='.', index = False,encoding="utf-8")
-'''
+data_.to_csv('/path/data_pred_150_2.csv', sep = ';', decimal='.', index = False,encoding="utf-8")
+
 ##############################         
 ##############################
 ###  Creating the same type of patches but only the 'raw', so basicaly we just cut the images in the correct form. 
@@ -384,8 +357,8 @@ coordinates[:,1]= y1_list                  # adding y coordinates to 1st column
     
 plt.scatter(coordinates[:,0],coordinates[:,1],s=markerSize,cmap=plt.cm.jet)# we can plot the coordinates to check if everything is ok
 #  Saving the original coordinates as *.csv file and created pacth as *.tif
-pd.DataFrame(coordinates).to_csv('/Users/hannamanko/Desktop/diff_st/My Library_2/U-net/for figures/coordinates_5.0.csv',index = False)
-imsave('/Users/hannamanko/Desktop/diff_st/My Library_2/U-net/for figures/50mW_4_patch_m5.0.tif', patch_x)
+pd.DataFrame(coordinates).to_csv('/Upath/coordinates.csv',index = False)
+imsave('/path/50mW_4_patch.tif', patch_x)
 ##############################  
 
 
@@ -393,8 +366,8 @@ imsave('/Users/hannamanko/Desktop/diff_st/My Library_2/U-net/for figures/50mW_4_
 #########   to create noisy patches
 #################################
 #  We can take any of the stacks used to create patches for the training set
-stack = imread('D:/1/Data/GattaQuant/Gqaunt_ Fluoro150mW_sPAINT__11/Gqaunt_ Fluoro150mW_sPAINT__11_MMStack_Pos0.ome.tif')
-stack = stack/60000
+stack = imread('/path/Gqaunt_ Fluoro150mW_sPAINT__11_MMStack_Pos0.ome.tif')
+stack = stack/2**16
 #########    To create big set of noisy patches this proces need to be repeated several times 
 #########    and obtained sets need to be concatenated 
 ###   :::::::::
@@ -410,7 +383,7 @@ for i in range(0, len(stack)):
             x2 = rand_x+half_width
             ## coordinates for spectral part
             y11 = rand_y+loc_sp_distance
-            y21 = rand_y+loc_sp_distance + sp_image_width
+            y21 = rand_y+loc_sp_distance + sp_image_height
             patch = np.concatenate((np.float64(stack[i, x1:x2, y1:y2]), 
                             np.float64(stack[i, x1:x2, y11:y21])), axis=1)
             try:
@@ -429,8 +402,8 @@ for i in range(0, len(stack)):
             count = count+1
             print(count)
                        
-patch__ = imread('D:/My Library_2/U-net/For_Network/xx/Noise3.tif') 
-patch_sum_ = imread('D:/My Library_2/U-net/For_Network/yy/Noise3.tif') 
+patch__ = imread('/path/Noise3.tif') 
+patch_sum_ = imread('/path/Noise3.tif') 
 
 
 #  Filtering and deleting the patches that randomly got localization
@@ -453,102 +426,12 @@ patch_sum_ = np.concatenate((patch_sum, ps), axis=0)
 #########  
 
 
-###################################################
-#################     To draw rectangles 
-###################################################
-
-from matplotlib.patches import Rectangle
-d= imread('D:/1/Data/GattaQuant/Gqaunt_ Fluoro75mW_sPAINT__7/Gqaunt_ Fluoro75mW_sPAINT__7_MMStack_Pos0.ome.tif')
-
-
-loc = pd.read_csv('C:/Users/hmanko/Desktop/1.csv')
-dd = d[64]
-
-cord = pd.concat((loc['origX'], loc['origY']), axis = 1, ignore_index=False)
-plt.scatter(loc['origX'], loc['origY'])
-
-y1 = cord.iloc[1,1]-half_width
-x1 = cord.iloc[1,0]-half_width
-
-plt.imshow(dd)
-ax = plt.gca()
-
-# Create a Rectangle patch
-rect = Rectangle((x1,y1),16,16,linewidth=1,edgecolor='r',facecolor='none')
-
-# Add the patch to the Axes
-ax.add_patch(rect)
-
-for i in range(15,25):
-    y1 = cord.iloc[i,1]-half_width
-    x1 = cord.iloc[i,0]-half_width
-    y11 = cord.iloc[i,0]+loc_sp_distance
-    plt.imshow(dd)
-    plt.gca().add_patch(Rectangle((x1,y1),16,16,linewidth=1,edgecolor='r',facecolor='none'))
-    plt.gca().add_patch(Rectangle((y11,y1),48,16,linewidth=1,edgecolor='green',facecolor='none'))
-plt.show()
-
-
-
-
-
- 
-##############################   
-one_p = patch__ 
-one_ps = patch_sum_ 
- 
-
-patch__ = np.concatenate((one_p,two_p), axis=0)
-   # one_p[:1500],two_p[:1500],tre_p[:1500], four_p[:1500], five_p[:1500],six_p[:1500], sev_p[:1500]), axis=0)
-patch_sum_ = np.concatenate((one_ps,two_ps), axis=0)
-
-plot_value(86)
-##########     //%% ploting and discarding patches with shitty profile   %%\\
-#########    DEpending on kind of images this step need to be adjusted
-spectra = patch__
-image_n_list = []
-y = list(range(0,spectra.shape[2]))
-for n in range(0,len(spectra)):
-    sum_sp = np.sum(spectra[n,3:12], axis=0)
-    fitter = modeling.fitting.LevMarLSQFitter()
-    model = Gaussian1D(100,0.1,0.1)   # depending on the data you need to give some initial values
-    fitted_model = fitter(model, y, sum_sp)
-    #print("amp: ",fitted_model.amplitude.value, "mean: ", fitted_model.mean.value, "stdev: ",fitted_model.stddev.value, "__",n)
-    if int(fitted_model.mean.value) not in range(10,48):
-        image_n_list.append(n)
-        #print("da", n)
-spectra_f = np.delete(spectra, image_n_list, 0)    
-patch__ = np.delete(patch__, image_n_list,0)
-patch_sum_ = np.delete(patch_sum_, image_n_list,0)
-
-imsave('/Users/hannamanko/Desktop/spectra.tif', spectra)
-
-####### Lets try to threshold a bit ?
-#
-im = imread('D:/My Library/U-net/patches/yy/patch_sp_sum_5000.tif')
-
-num = random.randint(0,im.shape[0])
-plt.imshow(im[num,:,:])
-img = im[num,:,:]
-
-plt.hist(img)
-imax = img.max()
-imin = img.min()
-for i in range(0, img.shape[0]):
-    for j in range(0, img.shape[1]-1):
-        if img[i,j] > imax-(imax/5):
-            print(i, j)
-            img[i,j] = img[i,j]*1.2
-plt.imshow(img)
-
-plt.plot(img[7,:])
-
 
 #################################
 #########   to create noisy patches
 #################################
 
-stack = imread('D:/1/Data/GataQuand__15-150mW/75mW_2/75mW_2_MMStack_Pos0.ome.tif')
+stack = imread('/path/75mW_2_MMStack_Pos0.ome.tif')
 stack = stack/stack.max()
 
      
@@ -572,14 +455,14 @@ patch_sum = patch_sum_
 patch_ = patch__
 
 
-imsave('D:/My Library_2/U-net/For_Network/yy/Noise3.tif', patch_sum_)
-imsave('D:/My Library_2/U-net/For_Network/xx/Noise3.tif', patch__)
+imsave('/path/Noise3.tif', patch_sum_)
+imsave('/path/Noise3.tif', patch__)
 
 patch_sum_ = np.concatenate((ps,ps2,ps3), axis=0)
 patch__ = np.concatenate((p,p2,p3), axis=0)
 
-noise_y = imread('D:/My Library_2/U-net/patches/yy/Noise.tif')
-noise_x = imread('D:/My Library_2/U-net/patches/xx/Noise.tif')
+noise_y = imread('/path/Noise.tif')
+noise_x = imread('/path/Noise.tif')
 
 
 ###################################################
@@ -587,10 +470,10 @@ noise_x = imread('D:/My Library_2/U-net/patches/xx/Noise.tif')
 ###################################################
 
 from matplotlib.patches import Rectangle
-d= imread('D:/1/Data/GattaQuant/Gqaunt_ Fluoro75mW_sPAINT__7/Gqaunt_ Fluoro75mW_sPAINT__7_MMStack_Pos0.ome.tif')
+d= imread('/path/Gqaunt_ Fluoro75mW_sPAINT__7_MMStack_Pos0.ome.tif')
 
 
-loc = pd.read_csv('C:/Users/hmanko/Desktop/1.csv')
+loc = pd.read_csv('/path/loc.csv')
 dd = d[64]
 
 cord = pd.concat((loc['origX'], loc['origY']), axis = 1, ignore_index=False)
@@ -616,8 +499,5 @@ for i in range(15,25):
     plt.gca().add_patch(Rectangle((x1,y1),16,16,linewidth=1,edgecolor='r',facecolor='none'))
     plt.gca().add_patch(Rectangle((y11,y1),48,16,linewidth=1,edgecolor='green',facecolor='none'))
 plt.show()
-
-def consecutive(data, stepsize=1):
-    return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
 
 
